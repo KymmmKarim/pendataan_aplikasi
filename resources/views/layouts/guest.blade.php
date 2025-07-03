a78c5cb2e95e71e018a6b9967ff3837a918f781e
Muhammad Sahel Muhyidin <akk28510@gmail.com>
2025-07-03 10:42:52 +0700

    feat(PAI-1):create login and register

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>Pendataan Aplikasi</title>
+        <title>Pendataan Aplikasi ITENAS</title>
         <link rel="icon" href="{{ asset('img/logo_itenas.png') }}" type="image/png">
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
